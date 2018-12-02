bd267c601f201ed1a952ac62e0533b40065cc892
Peter <peterjiang823@gmail.com>
2018-12-02 13:56:10 -0800

    added events and users table

--- models/tables.py
+++ models/tables.py
@@ -12,3 +12,30 @@
 
 # after defining tables, uncomment below to enable auditing
 # auth.enable_record_versioning(db)
+
+import datetime
+
+def get_user_email():
+    return None if auth.user is None else auth.user.email
+
+def get_current_time():
+    return datetime.datetime.utcnow()
+
+db.define_table('events',
+                Field('creator_email', default=get_user_email()),
+                Field('creator_name'),
+                Field('event_title'),
+                Field('event_description', 'text'),
+                Field('post_time', 'datetime', default=get_current_time()),
+                Field('event_category'),
+                Field("size_limit"),
+                )
+
+db.define_table('users',
+                Field('user_email', default=get_user_email()),
+                Field('user_name'),
+                Field('birthday'),
+                Field("university"),
+                Field("user_password"),
+
+                )
